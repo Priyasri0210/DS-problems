0366a4c819bce42da15cd729f9304b4a87ae7264
Priyasri-0210 <sripriyaravi6@gmail.com>
2025-03-31 21:04:54 +0530

    second commit

--- hw4/hw4(1).py
+++ hw4/hw4(1).py
@@ -2,7 +2,7 @@ student = {"name":"Alice", "age":20,"marks":85}
 print (student["name"])
 student['marks'] = 90
 print(student)
-student["subject"] = "math"
+student["subject"] = "maths"
 print(student)
 
 
